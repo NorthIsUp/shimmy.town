f5d1dcb4dfaed52c8e93e21bd77704a7f60d3de6
Adam Hitchcock <adam@northisup.com>
2017-10-03 12:43:17 -0700

    add gzip storage

--- shimmytown/shimmy/core/settings/pipeline.py
+++ shimmytown/shimmy/core/settings/pipeline.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+
 from .env import log_setting
 
 
--- shimmytown/shimmy/core/storage.py
+++ shimmytown/shimmy/core/storage.py
@@ -2,7 +2,16 @@
 from __future__ import absolute_import
 
 # External Libraries
+from pipeline.storage import (
+    GZIPMixin,
+    PipelineCachedStorage,
+)
 from storages.backends.s3boto import S3BotoStorage
+
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
+
+
+class GZIPPipelineCachedStorage(GZIPMixin, PipelineCachedStorage):
+    pass
