b170f9684d7cc85c4316c9ab8b6d74e37e66d6fe
Adam Hitchcock <adam@northisup.com>
2017-11-03 14:51:21 -0700

    fix manage.py proxy as a polyglot

--- manage.py
+++ manage.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env bash
-
-echo "proxying to shimmytown/manage.py" 1>&2
+''''echo "bash proxy to shimmytown/manage.py" 1>&2
 
 cd shimmytown
-./manage.py $@
+exec ./manage.py $@
+exit # just in case
+'''
+
+import os, sys
+print('python proxy')
+
+os.system('./manage.py ' + ' '.join(sys.argv[1:]))
